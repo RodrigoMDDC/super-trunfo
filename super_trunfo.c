f132da44a26029c8795c6c4f30df06ea76280802
RodrigoMDDC <rodrigaomd@gmail.com>
2025-04-17 03:07:19 +0000

    criação de duas cartas

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,105 @@
 
+#include <stdio.h>
+
+int main(){
+
+
+// Codigos da primeira carta
+
+    char estado_01;
+    int cod_carta_01;
+    char cidade_01[20];
+    int populacao_01;
+    float area_01;
+    float pib_01;
+    int pontos_turisticos_01;
+
+
+
+    printf("Entre com a classificação de estado da primeira carta: \n");
+    scanf(" %c", &estado_01);
+
+    printf("Entre com o codigo da carta: \n");
+    scanf(" %i", &cod_carta_01);
+
+    printf("Entre com o nome da cidade: \n");
+    scanf(" %s", cidade_01);
+
+    printf("Entre com a população da cidade: \n");
+    scanf(" %i", &populacao_01);
+
+    printf("Entre com a area da cidade em km2: \n");
+    scanf(" %f", &area_01);
+
+    printf("Entre com o PIB da Cidade: \n");
+    scanf(" %f", &pib_01);
+
+    printf("Entre com a quantidade de pontos turisticos da Cidade: \n");
+    scanf(" %i", &pontos_turisticos_01);
+
+
+
+    printf(" \n");
+
+    printf("Cartas de SUPER TRUNFO \n\n===Carta 01===\n\n");
+
+    printf("ESTADO: %c\n", estado_01);
+    printf("Codigo: %c-%i\n", estado_01, cod_carta_01);
+    printf("Cidade: %s\n", cidade_01);
+    printf("População: %i mil habitantes\n", populacao_01);
+    printf("Area (Km2): %.2f mil km2\n", area_01);
+    printf("PIB em milhões: R$ %.2f\n", pib_01);
+    printf("Pontos Turisticos: %i\n\n", pontos_turisticos_01);
+
+
+
+// codigo da segunda carta
+
+    char estado_02;
+    int cod_carta_02;
+    char cidade_02[20];
+    int populacao_02;
+    float area_02;
+    float pib_02;
+    int pontos_turisticos_02;
+
+
+
+    printf("Entre com a classificação de estado da segunda carta: \n");
+    scanf(" %c", &estado_02);
+
+    printf("Entre com o codigo da carta: \n");
+    scanf(" %i", &cod_carta_02);
+
+    printf("Entre com o nome da cidade: \n");
+    scanf(" %s", cidade_02);
+
+    printf("Entre com a população da cidade: \n");
+    scanf(" %i", &populacao_02);
+
+    printf("Entre com a area da cidade em km2: \n");
+    scanf(" %f", &area_02);
+
+    printf("Entre com o PIB da Cidade: \n");
+    scanf(" %f", &pib_02);
+
+    printf("Entre com a quantidade de pontos turisticos da Cidade: \n");
+    scanf(" %i", &pontos_turisticos_02);
+
+
+
+    printf(" \n");
+
+    printf("Cartas de SUPER TRUNFO \n\n===Carta 02===\n\n");
+
+    printf("ESTADO: %c\n", estado_02);
+    printf("Codigo: %c-%i\n", estado_01, cod_carta_02);
+    printf("Cidade: %s\n", cidade_02);
+    printf("População: %i mil habitantes\n", populacao_02);
+    printf("Area (Km2): %.2f mil km2\n", area_02);
+    printf("PIB em milhões: R$ %.2f\n", pib_02);
+    printf("Pontos Turisticos: %i\n\n", pontos_turisticos_02);
+
+
+
+}
