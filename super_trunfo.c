4a458727694794df9300811ac7b2d50ea0b0fefd
RodrigoMDDC <rodrigaomd@gmail.com>
2025-04-17 03:12:44 +0000

    correção no titulo da segunda carta

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,6 +6,8 @@ int main(){
 
 // Codigos da primeira carta
 
+// Declaração de variaveis da primeira carta
+
     char estado_01;
     int cod_carta_01;
     char cidade_01[20];
@@ -15,33 +17,41 @@ int main(){
     int pontos_turisticos_01;
 
 
+// Descrição do pedido com printf
+// Pedindo para que o usuario entre com os dados da primeira carta com scanf
 
     printf("Entre com a classificação de estado da primeira carta: \n");
-    scanf(" %c", &estado_01);
+    scanf(" %c", &estado_01);                                                          // Pedir pro usuario colocar o ESTADO da carta
 
     printf("Entre com o codigo da carta: \n");
-    scanf(" %i", &cod_carta_01);
+    scanf(" %i", &cod_carta_01);                                                       // Pedir pro usuario colocar o codigo da carta
 
     printf("Entre com o nome da cidade: \n");
-    scanf(" %s", cidade_01);
+    scanf(" %s", cidade_01);                                                           // Pedir pro usuario colocar a Cidade da carta
 
     printf("Entre com a população da cidade: \n");
-    scanf(" %i", &populacao_01);
+    scanf(" %i", &populacao_01);                                                       // Pedir pro usuario colocar a Quantidade populacional da carta
 
     printf("Entre com a area da cidade em km2: \n");
-    scanf(" %f", &area_01);
+    scanf(" %f", &area_01);                                                            // Pedir pro usuario colocar a Area da carta
 
     printf("Entre com o PIB da Cidade: \n");
-    scanf(" %f", &pib_01);
+    scanf(" %f", &pib_01);                                                             // Pedir pro usuario colocar O PIB da carta
 
     printf("Entre com a quantidade de pontos turisticos da Cidade: \n");
-    scanf(" %i", &pontos_turisticos_01);
+    scanf(" %i", &pontos_turisticos_01);                                               // Pedir pro usuario colocar a quantidade de Pontos Turisticos da carta
 
 
 
-    printf(" \n");
+    printf(" \n");      // apenas pulando uma linha
+    
+// Repetido o nome do jogo e informando a ordem da carta para a entrada dos dados 
 
     printf("Cartas de SUPER TRUNFO \n\n===Carta 01===\n\n");
+
+
+// Impressão dos dados da primeira carta conforme os dados de entrada que foram pedidos anteriormente
+
 
     printf("ESTADO: %c\n", estado_01);
     printf("Codigo: %c-%i\n", estado_01, cod_carta_01);
